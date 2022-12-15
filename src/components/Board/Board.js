37a51c5a8b9b899594d9f8bbc5a8ebf86e7af8d9
Dertex-js <ryoamir60@gmail.com>
2022-12-15 21:53:03 +0500

    save

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,7 +3,7 @@ import {useBoard} from "../../hooks"
 import cn from 'classnames'
 
 const Board = () => {
-  const { map, status, handleClear, handleStep } = useBoard()
+  const { map: matrix, status, handleClear, handleStep } = useBoard()
 
   return (
     <div className={cl.container}>
@@ -15,22 +15,40 @@ const Board = () => {
       </button>
       <div className={cl.board}>
         <ul className={cl.map}>
-          {map && map.map((field, index) => (
-            <li
-              className={cl.cell}
-              key={index}
-            >
-              <button
-                className={cn([
-                  cl.button,
-                  {[cl.disabled]: field !== ''},
-                  {[cl.finished]: status === 'finished'}
-                ])}
-                onClick={() => handleStep(index)}
-                disabled={(field !== '')}
-              >{field}</button>
-            </li>
+          {matrix && matrix.map((row, rowIndex) => (
+            row.map((field, fieldIndex) => (
+              <li
+                className={cl.cell}
+                key={fieldIndex}
+              >
+                <button
+                  className={cn([
+                    cl.button,
+                    {[cl.disabled]: field !== ''},
+                    {[cl.finished]: status === 'finished'}
+                  ])}
+                  onClick={() => handleStep({rowIndex, fieldIndex}, matrix)}
+                  disabled={(field !== '')}
+                >{field}</button>
+              </li>
+            ))
           ))}
+          {/*{map && map.map((field, index) => (*/}
+          {/*  <li*/}
+          {/*    className={cl.cell}*/}
+          {/*    key={index}*/}
+          {/*  >*/}
+          {/*    <button*/}
+          {/*      className={cn([*/}
+          {/*        cl.button,*/}
+          {/*        {[cl.disabled]: field !== ''},*/}
+          {/*        {[cl.finished]: status === 'finished'}*/}
+          {/*      ])}*/}
+          {/*      onClick={() => handleStep(index)}*/}
+          {/*      disabled={(field !== '')}*/}
+          {/*    >{field}</button>*/}
+          {/*  </li>*/}
+          {/*))}*/}
         </ul>
       </div>
     </div>
--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -44,17 +44,43 @@ const useBoard = () => {
       ws.send(JSON.stringify({ type: 'clearBoard' }))
     }
   }, [ws, isWs])
+  const handleStep = useCallback(({ rowIndex, fieldIndex }, matrix) => {
+    const normalise = (n, positive) => {
+      for (let i = 0; i < 4; i++) {
 
-  const handleStep = useCallback((field) => {
-    if (ws && isWs) {
-      if (steps.length === 0) {
-        ws.send(JSON.stringify({ type: 'firstStep', payload: {field} }))
-      } else {
-        ws.send(JSON.stringify({
-          type: 'step', payload: {field, prevStepId: steps.slice(-1)[0]?.id}
-        }))
       }
+       return positive
+         ? (n + 4 <= 19 ? n + 4 : n)
+         : (n - 4 >= 0 ? n - 4 : n)
     }
+
+    const winMatrix = []
+    let row = 0
+    let col = 0
+
+    for (let i = normalise(rowIndex, false); i <= normalise(rowIndex, true); i++) {
+      for (let j = normalise(fieldIndex, false); j <= normalise(fieldIndex, true); j++) {
+        if (fieldIndex - 4 >= 0 && fieldIndex + 4 <= 19) {
+          col = 9
+        } else {
+          col = 5
+        }
+        winMatrix[row] = new Array(col).fill(matrix[i][j])
+      }
+      // console.log(rowIndex, fieldIndex)
+      row++
+    }
+    console.log(winMatrix)
+
+    // if (ws && isWs) {
+    //   if (steps.length === 0) {
+    //     ws.send(JSON.stringify({ type: 'firstStep', payload: {field} }))
+    //   } else {
+    //     ws.send(JSON.stringify({
+    //       type: 'step', payload: {field, prevStepId: steps.slice(-1)[0]?.id}
+    //     }))
+    //   }
+    // }
   }, [ws, isWs, steps])
 
   return {map, steps, status, handleClear, handleStep}
